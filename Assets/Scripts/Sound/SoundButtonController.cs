f8e134de4f0ff21d604a393d1925092b58be4950
Marcello Vaz <mvaz@id.uff.br>
2016-06-08 22:26:21 -0300

    Menu reformulado. Adicionado botão de som.

---
+++ Assets/Scripts/Sound/SoundButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundButtonController : MonoBehaviour {
+
+    public Sprite enabledButton;
+    public Sprite disabledButton;
+
+    private Image currentImage;
+    private bool soundEnabled;
+
+    void Awake() {
+        currentImage = GetComponent<Image>();
+    }
+
+    public void EnableSound() {
+        soundEnabled = !soundEnabled;
+        currentImage.sprite = soundEnabled ? disabledButton : enabledButton;
+    }
+
+}
