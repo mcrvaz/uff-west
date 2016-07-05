ce3158bf482c31eb6e74d4e805e7332d77bb97af
Marcello Vaz <mcrvaz@hotmail.com>
2016-07-05 12:56:25 -0300

    Instruções atualizadas. Inicio da implementação de leitura e escrita das estatisticas.

---
+++ Assets/Scripts/DAO/Statistics/Statistics.cs
@@ -0,0 +1,46 @@
+﻿public class Statistics {
+
+    public int playerTargetsHit;
+    public int enemyTargetsHit;
+    public int playerDoubleDamageHit;
+    public int enemyDoubleDamageHit;
+    public int playerBulletTimeHit;
+    public int enemyBulletTimeHit;
+    public int targetsExpired;
+    public int playerShots;
+    public int enemyShots;
+    public float timeElapsed;
+    public float timeRemaining;
+    public float playerTimeBetweenShots;
+    public float enemyTimeBetweenShots;
+    public float playerShotsPerSecond;
+    public float enemyShotsPerSecond;
+
+    public Statistics() { }
+
+    public Statistics(
+        int playerTargetsHit, int enemyTargetsHit,
+        int playerDoubleDamageHit, int enemyDoubleDamageHit,
+        int playerBulletTimeHit, int enemyBulletTimeHit,
+        int targetsExpired, int playerShots, int enemyShots,
+        float timeElapsed, float timeRemaining,
+        float playerTimeBetweenShots, float enemyTimeBetweenShots,
+        float playerShotsPerSecond, float enemyShotsPerSecond
+    ) {
+        this.playerTargetsHit = playerTargetsHit;
+        this.enemyTargetsHit = enemyTargetsHit;
+        this.playerDoubleDamageHit = playerDoubleDamageHit;
+        this.enemyDoubleDamageHit = enemyDoubleDamageHit;
+        this.playerBulletTimeHit = playerBulletTimeHit;
+        this.enemyBulletTimeHit = enemyBulletTimeHit;
+        this.targetsExpired = targetsExpired;
+        this.playerShots = playerShots;
+        this.enemyShots = enemyShots;
+        this.timeElapsed = timeElapsed;
+        this.timeRemaining = timeRemaining;
+        this.playerTimeBetweenShots = playerTimeBetweenShots;
+        this.enemyTimeBetweenShots = enemyTimeBetweenShots;
+        this.playerShotsPerSecond = playerShotsPerSecond;
+        this.enemyShotsPerSecond = enemyShotsPerSecond;
+    }
+}
---
+++ Assets/Scripts/DAO/Statistics/StatisticsXMLContainer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+public class StatisticsXMLContainer : XMLContainer<StatisticsXMLContainer, Statistics> {
+    public List<Statistics> statistics = new List<Statistics>();
+    [XmlIgnore]
+    public string path;
+
+    public StatisticsXMLContainer() { } //Needed for XMLSerializer
+
+    public StatisticsXMLContainer(string fileName) {
+        path = Path.Combine(base.basePath, "Statistics");
+        path = Path.Combine(path, fileName);
+    }
+
+    public void Save() {
+        base.Save(this.path);
+    }
+
+    public void Load() {
+        var loaded = base.Load(this.path);
+        statistics = loaded.statistics;
+    }
+
+    public override void Push(Statistics statistic) {
+        statistics.Add(statistic);
+    }
+
+    public override void Remove(Statistics statistic) {
+        statistics.Remove(statistic);
+    }
+
+
+}
